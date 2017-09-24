3b1e45d5e721b5114e2314299a424368194d7abf
vkrauze <vkrauze@users.noreply.github.com>
2017-09-24 22:25:09 +0300

    Added GraphicsDataTest.

--- test/com/vkrauze/graphicseditor/display/GraphicsDataTest.java
+++ test/com/vkrauze/graphicseditor/display/GraphicsDataTest.java
@@ -1,39 +1,63 @@
 package com.vkrauze.graphicseditor.display;
 
+import com.vkrauze.graphicseditor.figure.GeometricFigure;
 import com.vkrauze.graphicseditor.figure.Line;
 import org.junit.Test;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static java.awt.Color.BLACK;
+import static java.awt.Color.WHITE;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class GraphicsDataTest {
+    private BufferedImage image;
+    private int imageWidth;
+    private int imageHeight;
 
     @Test
     public void testLineOutputIsRight() {
-        BufferedImage image = new BufferedImage(640, 480, BufferedImage.TYPE_INT_RGB);
-        Graphics graphics = image.getGraphics();
-        GraphicsData graphicsData = new GraphicsData(Collections.singletonList(new Line(1, 1, 3, 3)));
-        graphicsData.setCanvasWidth(4);
-        graphicsData.setCanvasHeight(4);
+        imageWidth = 3;
+        imageHeight = 4;
+        image = getImage();
+        GraphicsData graphicsData = new GraphicsData(getFiguresWithLine());
+        graphicsData.setCanvasWidth(imageWidth);
+        graphicsData.setCanvasHeight(imageHeight);
 
-        graphicsData.processFigures(graphics);
+        graphicsData.processFigures(image.getGraphics());
 
-//        assertEquals(Color.WHITE, getColor(image, 0,0));
-//        assertEquals(Color.BLACK, getColor(image, 1,0));
-//        assertEquals(Color.BLACK, getColor(image, 2,0));
-
-        int width = image.getWidth();
-        int height = image.getHeight();
-
-        int[] pixels = image.getRGB(0,0, width, height, null,0, width);
+        Color[] expectedPixels = getExpectedPixels();
+        Color[] actualPixels = getActualPixels();
+        assertEquals("Lengths of arrays must be equal", expectedPixels.length, actualPixels.length);
+        assertArrayEquals("Arrays must be equal", expectedPixels, actualPixels);
     }
 
-    private Color getColor(BufferedImage image, int x, int y) {
-        return new Color(image.getRGB(x,y));
+    // By default, background color is black and painting color is white
+    private Color[] getExpectedPixels() {
+        return new Color[]{BLACK, BLACK, BLACK,
+                BLACK, BLACK, WHITE,
+                BLACK, WHITE, BLACK,
+                WHITE, BLACK, BLACK};
     }
 
+    private Color[] getActualPixels() {
+        int[] actualRGB = image.getRGB(0, 0, imageWidth, imageHeight, null, 0, imageWidth);
+        return IntStream.of(actualRGB)
+                .mapToObj(rgb -> new Color(rgb))
+                .collect(Collectors.toList())
+                .toArray(new Color[]{});
+    }
+
+    private java.util.List<GeometricFigure> getFiguresWithLine() {
+        return Collections.singletonList(new Line(0, 1, 2, 3));
+    }
+
+    private BufferedImage getImage() {
+        return new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB);
+    }
 }
